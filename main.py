40e85e25176b0988fe94e9bcf44c26ca1cf94364
Mahir580 <88188721+Mahir580@users.noreply.github.com>
2021-09-09 21:20:13 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root= Tk()
+root.title("Clock")
+
+def time():
+    string=strftime("%I:%M:%S %p")
+    label.config(text=string)
+    label.after(1000,time)
+    
+
+label= Label(root, font=("Arial",80), background="black", foreground="crimson")
+label.pack(anchor="center")
+time()
+
+mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
